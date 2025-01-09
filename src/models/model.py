bd4e87aaae270481182a0b013748c25985b198e6
JustCati <ale.catinello@gmail.com>
2025-01-09 14:12:51 +0100

    feat: train yolo

---
+++ src/models/model.py
@@ -0,0 +1,38 @@
+import os
+import multiprocessing
+from ultralytics import YOLOv10
+
+
+def getYOLO(checkpoint_path: str, device: str = 'cpu') -> YOLOv10:
+    if not os.path.exists(checkpoint_path):
+        raise ValueError('Checkpoint path does not exist')
+    model = YOLOv10(checkpoint_path)
+    model.to(device)
+    return model
+
+
+def train(model: YOLOv10, 
+          yaml_file: str,
+          epochs: int,
+          patience: int,
+          batch_size: int,
+          model_path: str,
+          folder_name: str,
+          resume: bool = False,
+          device: str = 'cuda'):
+    cpu_workers = multiprocessing.cpu_count()
+    results = model.train(data=yaml_file, 
+                          batch=batch_size,
+                          imgsz=1024,
+                          epochs=epochs if patience == 0 else 10000,
+                          verbose=True,
+                          patience=patience,
+                          workers=cpu_workers,
+                          save_period=5,
+                          device=device,
+                          resume=resume,
+                          val=True,
+                          plots=True,
+                          project = model_path,
+                          name = folder_name)
+    return results

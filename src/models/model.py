a42f63bf59e0dff16355281b690add79e8541c24
JustCati <ale.catinello@gmail.com>
2025-01-11 09:21:14 +0100

    fix: remove model to device and leave all to library device assignation

--- src/models/model.py
+++ src/models/model.py
@@ -7,7 +7,6 @@ def getYOLO(checkpoint_path: str, device: str = 'cpu') -> YOLOv10:
     if not os.path.exists(checkpoint_path):
         raise ValueError('Checkpoint path does not exist')
     model = YOLOv10(checkpoint_path)
-    model.to(device)
     return model
 
 
